5c1b7dee18d95a8d6461622575604308f02bc89e
James Huang <m3ctdu49jo@gmail.com>
2024-10-25 14:03:11 +0800

    feat: News 輪播特效完成

--- ex01/src/components/NewsLoopBox.jsx
+++ ex01/src/components/NewsLoopBox.jsx
@@ -1,47 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import style from "../styles/style.module.css";
 
 const NewsWrap = styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
     width: 100%;
     max-width: 680px;
     height: 400px;
-    margin: 0 auto;
+    margin: 100px auto 0;
 
     ul {
+        width: 100%;
+        height: 230px;
+        opacity: 1;
+        transition: transform 1s ease-out, opacity 1s ease-out;
+        position: relative;
+        overflow: hidden;
+        border: 1px solid #eee;
     }
 
     li {
         background: #eee;
         display: block;
-        margin-bottom: 10px;
+        width: 80%;
+        margin: 0 auto;
         padding: .8rem .8rem;
         border-radius: 5px;
+        transition: transform 1s ease-out, opacity 1s ease-out;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+    li: first-child {
+
     }
 `;
 
-function fetchData(){
+async function fetchData(){
     let data;
-    return fetch("https://jsonplaceholder.typicode.com/posts", {
+    return await fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "get"
     }).then(response => response.json());
 }
 
-// 共用函式抽離
-function displayNewsItemsWithIndex(newsData, currentNewsId, setNewsItems){
+// 函式抽離
+function InitDisplayNewsItems(newsData, currentNewsId, setNewsItems, isInit = false){
     const displayedCount = 5;
-    let currentNewsIndex;            
-    if (currentNewsId !== 0){
-        currentNewsIndex = newsData.findIndex(x => x.id === currentNewsId);
-    } else{
-        currentNewsIndex = 0;
-    }
+    let currentNewsIndex;   
+    let items = [];         
+    
+    currentNewsIndex = currentNewsId !== 0 ? newsData.findIndex(x => x.id === currentNewsId) : 0;
 
-    let items = [];
     const newsCount = newsData.length;
+    
+    if(isInit)
+        items.push(newsData[newsCount - 1]);
     for(let i = 0;items.length < displayedCount;){
         // 若超過資料筆數上限，改得知目前超出筆數，透過下方 n + i 換算得知為資料的 1 ~ n 筆
         let n = currentNewsIndex + i >= newsCount ? currentNewsIndex - newsCount : currentNewsIndex;
@@ -49,29 +64,31 @@ function displayNewsItemsWithIndex(newsData, currentNewsId, setNewsItems){
         i++;
     }
     setNewsItems(items);
-    return currentNewsIndex;
 }
 
 
 function NewsLoopBox(){
     const [currentNewsId, setCurrentNewsId] = useState(0);
     const [newsItems, setNewsItems] = useState([]);
-    const [shouldFetch, setShouldFetch] = useState(false);
+    const [shouldFetch, setShouldFetch] = useState(true);
     const [newsArray, setNewsArray] = useState([]);
+    const newsItemRef = useRef([]);
+    const [trans, setTrnas] = useState(false);
+    const [elmsTop, setElmsTop] = useState([]);
 
     useEffect(() => {
-        if(!shouldFetch){
+        if(shouldFetch){
             fetchData().then(data => { 
-                displayNewsItemsWithIndex(data, currentNewsId, setNewsItems);
+                InitDisplayNewsItems(data, currentNewsId, setNewsItems, newsItems.length === 0);
                 setNewsArray(data);
-                setShouldFetch(true);
+                setTrnas(true); 
+                setShouldFetch(false);
             });
         }
     }, [shouldFetch]);
 
-    useEffect(() => {        
+    useEffect(() => {      
         const id = setInterval(() => {
-            const displayedCount = 5;
             let currentNewsIndex;            
             if (currentNewsId !== 0){
                 currentNewsIndex = newsArray.findIndex(x => x.id === currentNewsId);
@@ -79,33 +96,41 @@ function NewsLoopBox(){
                 currentNewsIndex = newsArray.findIndex(x => x.id === newsItems[newsItems.length - 1].id);
             }
 
-            let items = newsItems;
+            let items = [...newsItems];
             const newsCount = newsArray.length;
-            // for(let i = 0;items.length < displayedCount;){
-            //     // 若超過資料筆數上限，改得知目前超出筆數，透過下方 n + i 換算得知為資料的 1 ~ n 筆
-            //     let n = currentNewsIndex + i >= newsCount ? currentNewsIndex - newsCount : currentNewsIndex;
-            //     items.push(newsArray[n + i]);
-            //     i++;
-            // }
-
             
             let addItemIndex = currentNewsIndex + 1 === newsCount ? 0 : currentNewsIndex + 1;
             items.push(newsArray[addItemIndex]);
 
 
-
-
-
+            if(!shouldFetch)
+                items = items.slice(1);
+ 
             setNewsItems(items);
+            setTrnas(true); //重新計算間隔
             // 從下一個id開始取
             setCurrentNewsId(newsArray[currentNewsIndex + 1 === newsArray.length ? 0 : currentNewsIndex + 1].id);
-        }, 2000);
+        }, 5000);
         return () => { 
             clearTimeout(id);
-            newsItems.splice(0);
-            setNewsItems(newsItems => newsItems.splice(0));
         };
     }, [shouldFetch, currentNewsId]);
+
+
+    useEffect(() => {
+        let newElmsTop = [];
+        let m = 0;
+        newsItemRef.current.map((elm, index) => {
+            let mrg = 20;
+            let firstElemMove = (elm.offsetHeight + (mrg / 2)) * -1;
+            if((index === 1 || index === 4))
+                mrg = 40;
+            m += (elm.previousElementSibling ? (elm.previousElementSibling.offsetHeight + mrg) : firstElemMove);
+            newElmsTop.push(m);
+        });
+        setElmsTop(newElmsTop);
+        setTrnas(false);
+    }, [trans]);
 
 
 
@@ -113,7 +138,10 @@ function NewsLoopBox(){
         <NewsWrap>
             <ul>
                 {
-                    newsItems.map((news) => <li key={news.id}>{ news.id + news.title }</li>)
+                    newsItems.map((news, index) => {
+                        let top = elmsTop ? elmsTop[index] : 0;
+                        return <li key={news.id} ref={el => newsItemRef.current[index] = el} style={{transform: `translateY(${top}px)`}}>{ news.id + news.title }</li>
+                    })
                 }
             </ul>
         </NewsWrap>
