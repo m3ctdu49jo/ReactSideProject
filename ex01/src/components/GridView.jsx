e1972a8aecaa35345983f57820900b8301210f7c
DESKTOP-63VEBKQ\m3ctd <m3ctdu49jo@gmail.com>
2024-10-29 21:11:07 +0800

    feat: GridView排序功能編寫

--- ex01/src/components/GridView.jsx
+++ ex01/src/components/GridView.jsx
@@ -37,6 +37,21 @@ const ColumnNone = styled.div`
 //     columnName: "訂單單號"
 // }
 
+function mutiSort(data, sortConditions) {
+    console.log(sortConditions);
+    return data.sort((a, b) => {
+        for (let condition of sortConditions) {
+            const { key, asc = false } = condition;
+            const diff = (a[key] > b[key]) - (a[key] < b[key]);
+
+            if (diff !== 0) {
+                return asc ? diff : -diff;
+            }
+        }
+        return 0; // 所有屬性都相同時保持原順序
+    });
+}
+
 
 function GridView(data = [], columnsName = []) {
     const [dataItems, setDataItems] = useState([]);
@@ -44,14 +59,17 @@ function GridView(data = [], columnsName = []) {
     const [colsName, setColsName] = useState([]);
     const [colsId, setColsId] = useState([]);
     const [colsSort, setColsSort] = useState([]);
-    
+
 
     useEffect(() => {
         //fake
         let d = data[0] ? [...data] : [];
         let c = columnsName[0] ? [...columnsName] : [];
-        d.push({ orderNum: "A123456", orderSeq: "1", productId: "A1A356854", productName: "釣魚線" });
         d.push({ orderNum: "A123467", orderSeq: "5", productId: "A1A356866", productName: "網球線" });
+        d.push({ orderNum: "A123456", orderSeq: "1", productId: "A1A356854", productName: "釣魚線1" });
+        d.push({ orderNum: "A123456", orderSeq: "3", productId: "A1A356854", productName: "釣魚線2" });
+        d.push({ orderNum: "A123456", orderSeq: "2", productId: "A1A356854", productName: "釣魚線3" });
+        d.push({ orderNum: "A123456", orderSeq: "2", productId: "A1A356853", productName: "釣魚線3" });
         d.push({ orderNum: "A123468", orderSeq: "3", productId: "A1A356850", productName: "鞋面" });
         c.push({ colId: "orderNum", colName: "訂單單號" });
         c.push({ colId: "orderSeq", colName: "訂單項次" });
@@ -70,27 +88,30 @@ function GridView(data = [], columnsName = []) {
         }
     }, [data, columnsName]);
 
-    let changeDataSort = function (colId) {
+    let changeDataSort = async function (colId) {
         let newSort = [...colsSort];
-        let index = colsSort.findIndex(x => x.id === colId); 
-        let itemSorted = [...dataItems].sort((a, b) => {
-            if(index === -1 || (index >= 0 && !newSort[index].asc))
-                return a[colId].localeCompare(b[colId]);
-            else
-                return b[colId].localeCompare(a[colId]);
+        let index = colsSort.findIndex(x => x.key === colId);
 
-        });
-        setDataItems(itemSorted);
+        // 單個排序
+        // let itemSorted = [...dataItems].sort((a, b) => {
+        //     if(index === -1 || (index >= 0 && !newSort[index].asc))
+        //         return a[colId].localeCompare(b[colId]);
+        //     else
+        //         return b[colId].localeCompare(a[colId]);
+        // });
 
-        if(index >= 0){
+        if (index >= 0) {
             newSort[index].asc = !colsSort[index].asc;
         }
         else {
             newSort.push({
-                id: colId,
-                asc: true
+                key: colId,
+                asc: false
             });
         }
+        let itemSorted = await mutiSort([...dataItems], newSort);
+        console.log(itemSorted);
+        setDataItems(itemSorted);
         setColsSort(newSort);
     }
 
@@ -128,30 +149,3 @@ function GridView(data = [], columnsName = []) {
 }
 
 export default GridView;
-
-
-const people = [
-    { name: "Alice", age: 25 },
-    { name: "Bob", age: 20 },
-    { name: "Charlie", age: 30 }
-];
-
-function sortByProperty(array, property) {
-    return array.sort((a, b) => {
-        if (typeof a[property] === "string") {
-            return a[property].localeCompare(b[property]);
-        } else {
-            return a[property] - b[property];
-        }
-    });
-}
-
-
-// 使用函式進行排序，傳入屬性名稱 "name"
-const sortedByName = sortByProperty(people, "name");
-console.log(sortedByName);
-// 輸出: [
-//   { name: "Alice", age: 25 },
-//   { name: "Bob", age: 20 },
-//   { name: "Charlie", age: 30 }
-// ]
